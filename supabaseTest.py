529a2c2684c999714792abd86690621b5ee837c6
Jackson Morawski <129026989+jaxmorawski@users.noreply.github.com>
2023-09-21 11:33:02 -0500

    Update supabaseTest.py
    added decouple from env, still need to add client code

--- supabaseTest.py
+++ supabaseTest.py
@@ -1,5 +1,15 @@
 import os
 from supabase import create_client, Client
+from decouple import config
+
+# Read the variables from the .env file
+username = config('USERNAME')
+password = config('PASSWORD')
+
+# Use the variables as needed
+print(f'Username: {username}')
+print(f'Password: {password}')
+
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
