439ca0e03ba4f5a04e8f575fff6584a239f5dd0c
Jackson Morawski <129026989+jaxmorawski@users.noreply.github.com>
2023-09-21 14:27:30 -0500

    Update supabaseTest.py

--- supabaseTest.py
+++ supabaseTest.py
@@ -1,16 +1,20 @@
 import os
 from supabase import create_client, Client
-from decouple import config
+#from decouple import config
 
-# Read the variables from the .env file
-username = config('USERNAME')
-password = config('PASSWORD')
+# Read the variables from the .env file may not be needed
+#username = config('USERNAME')
+#password = config('PASSWORD')
 
 # Use the variables as needed
-print(f'Username: {username}')
-print(f'Password: {password}')
+#print(f'Username: {username}')
+#print(f'Password: {password}')
 
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("https://zjhemihkjcspikabnabb.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqaGVtaWhramNzcGlrYWJuYWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ1NDU4ODUsImV4cCI6MjAxMDEyMTg4NX0.OlzWaElvTMe1eUfFTkxSmG7y9xaKGDWgV5mMjlzDCuU")
+
 supabase: Client = create_client(url, key)
+
+#signing in implementation
+data = supabase.auth.sign_in_with_password({"email": "jlmoraws@uark.edu", "password": "5B2G6N~RWyz8hK"})
